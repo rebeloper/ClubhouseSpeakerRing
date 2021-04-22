2dbb9f04406fd7927e8b457ffc62e51fa0a44324
Alex Nagy <therebeloper@gmail.com>
2021-04-22 17:23:25 +0300

    Initial Commit

---
+++ ClubhouseSpeakerRing/ClubhouseSpeakerRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubhouseSpeakerRingApp.swift
+//  ClubhouseSpeakerRing
+//
+//  Created by Alex Nagy on 22.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct ClubhouseSpeakerRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
